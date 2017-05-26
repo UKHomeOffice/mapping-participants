eab18273610613ac68b63c3b98b7ee45e7bc4b15
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2017-05-26 10:25:25 +0100

    * Add a get request to home template * Add govuk templates to the partials * Change port to 4000 * Add lodash for a merge of locals * Remove comments

--- app.js
+++ app.js
@@ -2,19 +2,25 @@
 
 const express = require('express');
 const app = express();
-const path = require('path')// mustache govuk template
+const path = require('path')
 const govukTemplate = require('hof-govuk-template');
-const hoganExpressStrict = require('hogan-express-strict');// Allows the use of partials
+const hoganExpressStrict = require('hogan-express-strict');
 const expressPartialTemplates = require('express-partial-templates');
+const _ = require('lodash');
 
 govukTemplate.setup(app);
 app.set('view engine', 'html')
-app.set('views', path.resolve(__dirname, 'views'))
+app.set('views', path.resolve(__dirname, 'app/views'))
 app.engine('html', hoganExpressStrict);
 app.use(expressPartialTemplates(app));
 
 app.use('public', express.static(path.resolve(__dirname, 'public')))
 
-app.listen(3000, function() {
-  console.log('App on port 3000')
+app.get('/', function(req, res, next){
+  res.render('home', _.merge({}, res.locals, {
+  }))
+})
+
+app.listen(4000, function() {
+  console.log('App on port 4000')
 })
