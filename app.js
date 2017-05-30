4a090d2a477af216891773a748edde31fb74a1b7
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2017-05-30 15:39:03 +0100

    Make a API call and log the response on startup * Use axios for the api call as it uses promises * Add a config for api creds, baseurl, surveyID, endpoint * Build the api from the config

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const govukTemplate = require('hof-govuk-template');
 const hoganExpressStrict = require('hogan-express-strict');
 const expressPartialTemplates = require('express-partial-templates');
 const _ = require('lodash');
+const apiCall = require('./lib/api-call');
 
 govukTemplate.setup(app);
 app.set('view engine', 'html')
@@ -15,6 +16,8 @@ app.engine('html', hoganExpressStrict);
 app.use(expressPartialTemplates(app));
 
 app.use('public', express.static(path.resolve(__dirname, 'public')))
+
+apiCall.getResponses();
 
 app.get('/', function(req, res, next){
   res.render('home', _.merge({}, res.locals, {
