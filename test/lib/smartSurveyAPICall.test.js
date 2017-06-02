83b5b62beadbc646152078b8bd6ab61b5daec347
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2017-06-02 16:19:05 +0100

    Add getResponses tests simplify code in app.js and getResponses function

---
+++ test/lib/smartSurveyAPICall.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const apiCall = require('../../lib/smartSurveyAPICall');
+const config = require('../../config');
+
+describe('api-call', () => {
+  describe('getResponses', () => {   
+    //chris chat
+    it('assert that it has been called with the parameters baseURL blah blah', function() {
+      // mock axios, proxyRequire or rewire
+      // run the get method in lib doc
+      // assert axious was run with the expected parameters
+    });
+
+    // beforeAll setup the mock
+    //describe block
+    it('SmartSurveyAPItest: assert that methods have been called with expected Args ', function() {
+      // mock SmartSurveyAPItest, proxyRequire or rewire
+      // set up mock to expect the correct args and respond appropriately
+      // check the assertion works
+    });    
+
+    it('SmartSurveyAPItest: assert that methods have been called with not expected Args ', function() {
+      // mock SmartSurveyAPItest, proxyRequire or rewire
+      // set up mock to expect the correct args and respond appropriately
+      // check the assertion works
+    });    
+
+    it('SmartSurveyAPItest: when there is an error, log it out', function() {
+      // mock SmartSurveyAPItest, proxyRequire or rewire
+      // set up mock to expect the correct args and respond appropriately
+      // check the assertion works
+    });    
+}
+})
